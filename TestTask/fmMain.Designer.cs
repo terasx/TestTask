07bb800d7fdbe632e07650ffba441a9b4555738e
terasx <terasx@ukr.net>
2016-11-03 15:12:00 +0200

    d

---
+++ TestTask/fmMain.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace TestTask
+{
+    partial class fmMain
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnSimulation = new System.Windows.Forms.Button();
+            this.TaskTimer = new System.Windows.Forms.Timer(this.components);
+            this.progressSimulation = new System.Windows.Forms.ProgressBar();
+            this.tbReport = new System.Windows.Forms.TextBox();
+            this.laFilePath = new System.Windows.Forms.Label();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSimulation
+            // 
+            this.btnSimulation.Location = new System.Drawing.Point(12, 12);
+            this.btnSimulation.Name = "btnSimulation";
+            this.btnSimulation.Size = new System.Drawing.Size(209, 23);
+            this.btnSimulation.TabIndex = 0;
+            this.btnSimulation.Text = "Моделювати роботу підприємства";
+            this.btnSimulation.UseVisualStyleBackColor = true;
+            this.btnSimulation.Click += new System.EventHandler(this.btnSimulation_Click);
+            // 
+            // TaskTimer
+            // 
+            this.TaskTimer.Interval = 1;
+            this.TaskTimer.Tick += new System.EventHandler(this.TaskTimer_Tick);
+            // 
+            // progressSimulation
+            // 
+            this.progressSimulation.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressSimulation.Location = new System.Drawing.Point(237, 12);
+            this.progressSimulation.Maximum = 160;
+            this.progressSimulation.Name = "progressSimulation";
+            this.progressSimulation.Size = new System.Drawing.Size(387, 23);
+            this.progressSimulation.TabIndex = 1;
+            // 
+            // tbReport
+            // 
+            this.tbReport.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbReport.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbReport.Location = new System.Drawing.Point(12, 52);
+            this.tbReport.Multiline = true;
+            this.tbReport.Name = "tbReport";
+            this.tbReport.ReadOnly = true;
+            this.tbReport.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbReport.Size = new System.Drawing.Size(612, 317);
+            this.tbReport.TabIndex = 2;
+            // 
+            // laFilePath
+            // 
+            this.laFilePath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.laFilePath.AutoSize = true;
+            this.laFilePath.Location = new System.Drawing.Point(122, 384);
+            this.laFilePath.Name = "laFilePath";
+            this.laFilePath.Size = new System.Drawing.Size(0, 13);
+            this.laFilePath.TabIndex = 3;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnOpen.Enabled = false;
+            this.btnOpen.Location = new System.Drawing.Point(12, 379);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(104, 23);
+            this.btnOpen.TabIndex = 4;
+            this.btnOpen.Text = "Відкрити файл";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // fmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 410);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.laFilePath);
+            this.Controls.Add(this.tbReport);
+            this.Controls.Add(this.progressSimulation);
+            this.Controls.Add(this.btnSimulation);
+            this.Name = "fmMain";
+            this.Text = "Підприємство";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSimulation;
+        private System.Windows.Forms.Timer TaskTimer;
+        private System.Windows.Forms.ProgressBar progressSimulation;
+        private System.Windows.Forms.TextBox tbReport;
+        private System.Windows.Forms.Label laFilePath;
+        private System.Windows.Forms.Button btnOpen;
+    }
+}
+
---
+++ TestTask/fmMain.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestTask
+{
+
+    public partial class fmMain : Form
+    {
+        int TickCnt;
+
+        Firm firm = new Firm();
+
+        public fmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSimulation_Click(object sender, EventArgs e)
+        {
+            TaskTimer.Enabled = true;
+            TickCnt = 0;
+        }
+
+        private void TaskTimer_Tick(object sender, EventArgs e)
+        {
+            if (TickCnt < 160)
+            {
+                firm.GiveTask();
+                firm.CheckTask();
+                TickCnt++;
+                progressSimulation.Value = TickCnt;
+            }
+            else
+            {
+                TaskTimer.Enabled = false;
+                firm.MakeReport();
+
+                string strFileName = "report.txt";
+                StreamReader sr = new StreamReader(strFileName);
+                tbReport.Text = sr.ReadToEnd();
+                sr.Close();
+
+                btnOpen.Enabled = true;
+                laFilePath.Text = Environment.CurrentDirectory + "\\" + strFileName;
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(".\\report.txt");
+        }
+    }
+
+
+    public class Firm
+    {
+        int StaffCount;
+        int[,] Staff;
+        Random rnd;
+        int freelancer = 0;
+
+        const int DIRECTOR = 0;
+        const int ACCOUNTANT = 1;
+        const int MANAGER = 2;
+        const int DESIGNER = 3;
+        const int PROGRAMMER = 4;
+        const int TESTER = 5;
+        const int NONE = 6;
+
+        public Firm()
+        {
+            rnd = new Random();
+            StaffCount = rnd.Next(10, 100);
+            // должность:совмещение:зарплата:заданий выполнено:время занятости
+            Staff = new int[StaffCount, 5];
+
+            // Заполняем поле Директор
+            Staff[DIRECTOR, 0] = DIRECTOR;  // Должность
+            Staff[DIRECTOR, 1] = NONE;      // Совмещаемая должность
+            Staff[DIRECTOR, 2] = 10000;     // Зарплата
+            Staff[DIRECTOR, 3] = 0;         // Кол-во выполненых заданий
+            Staff[DIRECTOR, 4] = 0;         // Время до завершения задания
+
+            // Заполняем поле Бухгалтер
+            Staff[ACCOUNTANT, 0] = ACCOUNTANT;
+            Staff[ACCOUNTANT, 1] = NONE;
+            Staff[ACCOUNTANT, 2] = 8000;
+            Staff[ACCOUNTANT, 3] = 0;
+            Staff[ACCOUNTANT, 4] = 0;
+
+            // Генератор сотрудников
+            for (int i = 2; i < StaffCount; i++)
+            {
+                //должность
+                Staff[i, 0] = rnd.Next(2, 6);
+
+                //совмещаемая должность
+                int subpost = rnd.Next(2, 7);
+
+                if (subpost == Staff[i, 0])
+                {
+                    Staff[i, 1] = NONE;
+                }
+                else
+                {
+                    Staff[i, 1] = subpost;
+                }
+
+                switch (Staff[i, 0])
+                {
+                    case MANAGER: Staff[i, 2] = 6000;
+                        break;
+                    case DESIGNER: Staff[i, 2] = 4000;
+                        break;
+                    case PROGRAMMER: Staff[i, 2] = 5000;
+                        break;
+                    case TESTER: Staff[i, 2] = 4000;
+                        break;
+                }
+            }
+        }
+
+        public void GiveTask()
+        {
+            int TaskCount = rnd.Next(1, 2);
+            int cnt = TaskCount;
+            int TaskForStaff = rnd.Next(1,6);
+
+            while (cnt > 0)
+            {
+                for (int x = 0; x < StaffCount; x++)
+                {
+                    if (((Staff[x, 0] == TaskForStaff) | (Staff[x, 1] == TaskForStaff))
+                        && (Staff[x, 4] == 0))
+                    {
+                        Staff[x, 4] += TaskCount;
+                        continue;
+                    }
+                    else
+                    {
+                        freelancer += TaskCount;
+                    }
+                }
+
+                cnt--;
+            }
+        }
+
+        public void CheckTask()
+        {
+            for (int z = 0; z < StaffCount; z++)
+            {
+                if (Staff[z, 4] > 0)
+                {
+                    if (Staff[z, 4] >1)
+                    {
+                        Staff[z, 4]--;
+                    }
+
+                    if (Staff[z, 4] == 1)
+                    {
+                        Staff[z, 4]--;
+                        Staff[z, 3]++;
+                    }
+                }
+            }
+        }
+
+        public void MakeReport()
+        {
+            string data = "", post = "", subpost = "";
+            string strFileName = "report.txt";
+            FileInfo dataFile = new FileInfo(strFileName);
+            try
+            {
+                // використано клас StreamWriter
+                StreamWriter writeData = dataFile.CreateText();
+
+                writeData.WriteLine("Посада\t\tСумісництво\tОклад\tЗавдань\tЗарплата\r\n");
+                
+                for(int i = 0; i < StaffCount; i++)
+                {
+                    switch (Staff[i, 0])
+                    {
+                        case DIRECTOR: post = "Директор";
+                            break;
+                        case ACCOUNTANT: post = "Бухгалтер";
+                            break;
+                        case MANAGER: post = "Менеджер";
+                            break;
+                        case PROGRAMMER: post = "Програміст";
+                            break;
+                        case DESIGNER: post = "Дизайнер";
+                            break;
+                        case TESTER: post = "Тестувальник";
+                            break;
+                    }
+
+                    switch (Staff[i, 1])
+                    {
+                        case DIRECTOR: subpost = "Директор"; 
+                            break;
+                        case ACCOUNTANT: subpost = "Бухгалтер";
+                            break;
+                        case MANAGER: subpost = "Менеджер";
+                            break;
+                        case PROGRAMMER: subpost = "Програміст";
+                            break;
+                        case DESIGNER: subpost = "Дизайнер";
+                            break;
+                        case TESTER: subpost = "Тестувальник";
+                            break;
+                        case NONE: subpost = "   -//-   ";
+                            break;
+                    }
+
+                    data = String.Format("{0}\t{1}\t{2}\t{3}\t{4}", 
+                                        post, 
+                                        subpost,
+                                        Staff[i,2],
+                                        Staff[i,3],
+                                        Staff[i,3] * 10 + Staff[i,2]);
+
+                    writeData.WriteLine(data);
+                }
+
+                data = String.Format("\r\nФрилансер\t   -//-   \t-//-\t{0}\t{1}",
+                                     freelancer, freelancer * 10);
+
+                writeData.WriteLine(data);
+                writeData.Close();      // закриваємо поток
+            }
+            catch (IOException ex)
+            {
+                // повідомлення про сбій збереження даних
+                MessageBox.Show(ex.Message, "Помилка створення файла", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            
+        }
+
+    }
+
+
+}
